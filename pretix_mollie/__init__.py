0487c7659a8a97ccc5fdf3ddac6b8dddb87d8fdd
Raphael Michel <mail@raphaelmichel.de>
2018-08-14 11:36:29 +0200

    Initial commit

---
+++ pretix_mollie/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'pretix_mollie'
+    verbose_name = 'Mollie payment integration for pretix'
+
+    class PretixPluginMeta:
+        name = ugettext_lazy('Mollie payment integration for pretix')
+        author = 'Raphael Michel'
+        description = ugettext_lazy('Integration for the Mollie payment provider.')
+        visible = True
+        version = '1.0.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretix_mollie.PluginApp'
