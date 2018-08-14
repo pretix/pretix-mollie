3c22cfedb533c45c5dd62206188260d1ed604bbb
Raphael Michel <mail@raphaelmichel.de>
2018-08-14 17:37:45 +0200

    Implementation

--- pretix_mollie/signals.py
+++ pretix_mollie/signals.py
@@ -1,1 +1,116 @@
-# Register your receivers here
+import logging
+import time
+from collections import OrderedDict
+
+import requests
+from django import forms
+from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
+from pretix.base.models import Event_SettingsStore
+from pretix.base.settings import GlobalSettingsObject, settings_hierarkey
+from pretix.base.signals import (
+    logentry_display, periodic_task, register_global_settings,
+    register_payment_providers,
+)
+from pretix.helpers.urls import build_absolute_uri
+
+from .forms import MollieKeyValidator
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(register_payment_providers, dispatch_uid="payment_mollie")
+def register_payment_provider(sender, **kwargs):
+    from .payment import (
+        MollieSettingsHolder, MollieCC, MollieBancontact, MollieBelfius,
+        MollieBanktransfer, MollieBitcoin, MollieEPS,
+        MollieGiropay, MollieIdeal, MollieINGHomePay, MollieKBC,
+        MolliePaysafecard, MollieSofort
+    )
+
+    return [
+        MollieSettingsHolder,
+        MollieCC,
+        MollieBancontact,
+        MollieBanktransfer,
+        MollieBelfius,
+        MollieBitcoin,
+        MollieEPS,
+        MollieGiropay,
+        MollieIdeal,
+        MollieINGHomePay,
+        MollieKBC,
+        MolliePaysafecard,
+        MollieSofort
+    ]
+
+
+@receiver(signal=logentry_display, dispatch_uid="mollie_logentry_display")
+def pretixcontrol_logentry_display(sender, logentry, **kwargs):
+    if not logentry.action_type.startswith('pretix_mollie.event'):
+        return
+
+    plains = {
+        'canceled': _('Payment canceled.'),
+        'failed': _('Payment failed.'),
+        'paid': _('Payment succeeded.'),
+        'expired': _('Payment expired.'),
+        'disabled': _('Payment method disabled since we were unable to refresh the access token. Please '
+                      'contact support.'),
+    }
+    text = plains.get(logentry.action_type[20:], None)
+    if text:
+        return _('Mollie reported an event: {}').format(text)
+
+
+settings_hierarkey.add_default('payment_mollie_method_cc', True, bool)
+
+
+@receiver(register_global_settings, dispatch_uid='mollie_global_settings')
+def register_global_settings(sender, **kwargs):
+    return OrderedDict([
+        ('payment_mollie_connect_client_id', forms.CharField(
+            label=_('Mollie Connect: Client ID'),
+            required=False,
+            validators=(
+                MollieKeyValidator('app_'),
+            ),
+        )),
+        ('payment_mollie_connect_client_secret', forms.CharField(
+            label=_('Mollie Connect: Client secret'),
+            required=False,
+        )),
+    ])
+
+
+@receiver(periodic_task, dispatch_uid='mollie_refresh_tokens')
+def register_global_settings(sender, **kwargs):
+    seen = set()
+    gs = GlobalSettingsObject()
+    for es in Event_SettingsStore.objects.filter(key='payment_mollie_expires'):
+        if time.time() - float(es.object.settings.payment_mollie_expires) < 600:
+            rt = es.object.settings.payment_mollie_refresh_token
+            if rt not in seen:
+                try:
+                    resp = requests.post('https://api.mollie.com/oauth2/tokens', auth=(
+                        gs.settings.payment_mollie_connect_client_id,
+                        gs.settings.payment_mollie_connect_client_secret
+                    ), data={
+                        'grant_type': 'refresh_token',
+                        'refresh_token': es.object.settings.payment_mollie_refresh_token,
+                        'redirect_uri': build_absolute_uri('plugins:pretix_mollie:oauth.return')
+                    })
+                except:
+                    logger.exception('Unable to refresh mollie token')
+                    if float(es.object.settings.payment_mollie_expires) > time.time() and not \
+                            es.object.settings.payment_mollie_api_key:
+                        es.object.settings.payment_mollie__enabled = False
+                        es.object.log_action('pretix_mollie.event.disabled')
+                else:
+                    if resp.status_code == 200:
+                        data = resp.json()
+                        for ev in Event_SettingsStore.objects.filter(key='payment_mollie_refresh_token', value=rt):
+                            ev.object.settings.payment_mollie_access_token = data['access_token']
+                            ev.object.settings.payment_mollie_refresh_token = data['refresh_token']
+                            ev.object.settings.payment_mollie_expires = time.time() + data['expires_in']
+                        seen.add(rt)
---
+++ pretix_mollie/views.py
@@ -0,0 +1,291 @@
+import hashlib
+import json
+import logging
+import time
+from decimal import Decimal
+
+import requests
+from django.contrib import messages
+from django.core import signing
+from django.http import Http404, HttpResponse, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.utils.functional import cached_property
+from django.utils.translation import ugettext_lazy as _
+from django.views import View
+from django.views.decorators.clickjacking import xframe_options_exempt
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from pretix.base.models import Event, Order, OrderPayment, Quota
+from pretix.base.payment import PaymentException
+from pretix.base.services.locking import LockTimeoutException
+from pretix.base.settings import GlobalSettingsObject
+from pretix.control.permissions import event_permission_required
+from pretix.helpers.urls import build_absolute_uri
+from pretix.multidomain.urlreverse import eventreverse
+from requests import HTTPError
+
+logger = logging.getLogger(__name__)
+
+
+@xframe_options_exempt
+def redirect_view(request, *args, **kwargs):
+    signer = signing.Signer(salt='safe-redirect')
+    try:
+        url = signer.unsign(request.GET.get('url', ''))
+    except signing.BadSignature:
+        return HttpResponseBadRequest('Invalid parameter')
+
+    r = render(request, 'pretix_mollie/redirect.html', {
+        'url': url,
+    })
+    r._csp_ignore = True
+    return r
+
+
+def oauth_return(request, *args, **kwargs):
+    if 'payment_mollie_oauth_event' not in request.session:
+        messages.error(request, _('An error occurred during connecting with Mollie, please try again.'))
+        return redirect(reverse('control:index'))
+
+    event = get_object_or_404(Event, pk=request.session['payment_mollie_oauth_event'])
+
+    if request.GET.get('state') != request.session['payment_mollie_oauth_token']:
+        messages.error(request, _('An error occurred during connecting with Mollie, please try again.'))
+        return redirect(reverse('control:event.settings.payment.provider', kwargs={
+            'organizer': event.organizer.slug,
+            'event': event.slug,
+            'provider': 'mollie'
+        }))
+
+    gs = GlobalSettingsObject()
+
+    try:
+        resp = requests.post('https://api.mollie.com/oauth2/tokens', auth=(
+            gs.settings.payment_mollie_connect_client_id,
+            gs.settings.payment_mollie_connect_client_secret
+        ), data={
+            'grant_type': 'authorization_code',
+            'code': request.GET.get('code'),
+            'redirect_uri': build_absolute_uri('plugins:pretix_mollie:oauth.return'),
+        })
+        resp.raise_for_status()
+        data = resp.json()
+
+        if 'error' not in data:
+            orgaresp = requests.get('https://api.mollie.com/v2/organizations/me', headers={
+                'Authorization': 'Bearer ' + data.get('access_token')
+            })
+            orgaresp.raise_for_status()
+            orgadata = orgaresp.json()
+
+            profilesurl = 'https://api.mollie.com/v2/profiles'
+            profiles = []
+            while profilesurl:
+                profilesresp = requests.get(profilesurl, headers={
+                    'Authorization': 'Bearer ' + data.get('access_token')
+                })
+                profilesresp.raise_for_status()
+                d = profilesresp.json()
+                profiles += d['_embedded']['profiles']
+                if d['_links']['next']:
+                    profilesurl = d['_links']['next']['href']
+                else:
+                    profilesurl = None
+    except:
+        logger.exception('Failed to obtain OAuth token')
+        messages.error(request, _('An error occurred during connecting with Mollie, please try again.'))
+    else:
+        if 'error' in data:
+            messages.error(request, _('Mollie returned an error: {}').format(data['error_description']))
+        elif not profiles:
+            messages.error(request, _('Please create a website profile in your Mollie account and try again.'))
+        else:
+            messages.success(request,
+                             _('Your Mollie account is now connected to pretix. You can change the settings in '
+                               'detail below.'))
+            event.settings.payment_mollie_access_token = data['access_token']
+            event.settings.payment_mollie_refresh_token = data['refresh_token']
+            event.settings.payment_mollie_expires = time.time() + data['expires_in']
+            event.settings.payment_mollie_connect_org_id = orgadata.get('id')
+            event.settings.payment_mollie_connect_org_name = orgadata.get('name')
+            event.settings.payment_mollie_connect_profiles = [
+                [
+                    p.get('id'),
+                    p.get('name') + ' - ' + p.get('website', '')
+                ] for p in profiles
+            ]
+            event.settings.payment_mollie_connect_profile = profiles[0].get('id')
+
+            if request.session.get('payment_mollie_oauth_enable', False):
+                event.settings.payment_mollie__enabled = True
+                del request.session['payment_mollie_oauth_enable']
+
+    return redirect(reverse('control:event.settings.payment.provider', kwargs={
+        'organizer': event.organizer.slug,
+        'event': event.slug,
+        'provider': 'mollie'
+    }))
+
+
+def handle_payment(payment, mollie_id):
+    pprov = payment.payment_provider
+    if pprov.settings.connect_client_id and pprov.settings.access_token and pprov.settings.endpoint == "test":
+        qp = 'testmode=true'
+    else:
+        qp = ''
+    try:
+        resp = requests.get(
+            'https://api.mollie.com/v2/payments/' + mollie_id + '?' + qp,
+            headers=pprov.request_headers
+        )
+        resp.raise_for_status()
+        data = resp.json()
+
+        if data.get('amountRefunded') and data.get('status') == 'paid':
+            refundsresp = requests.get(
+                'https://api.mollie.com/v2/payments/' + mollie_id + '/refunds?' + qp,
+                headers=pprov.request_headers
+            )
+            refundsresp.raise_for_status()
+            refunds = refundsresp.json()['_embedded']['refunds']
+        else:
+            refunds = []
+
+        if data.get('status') == 'paid':
+            chargebacksresp = requests.get(
+                'https://api.mollie.com/v2/payments/' + mollie_id + '/chargebacks?' + qp,
+                headers=pprov.request_headers
+            )
+            chargebacksresp.raise_for_status()
+            chargebacks = chargebacksresp.json()['_embedded']['chargebacks']
+        else:
+            chargebacks = []
+
+        payment.info = json.dumps(data)
+        payment.save()
+
+        if payment.state in (OrderPayment.PAYMENT_STATE_CREATED, OrderPayment.PAYMENT_STATE_PENDING):
+            if data.get('status') == 'canceled':
+                payment.state = OrderPayment.PAYMENT_STATE_CANCELED
+                payment.save()
+                payment.order.log_action('pretix_mollie.event.canceled')
+            elif data.get('status') == 'pending' and payment.state == OrderPayment.PAYMENT_STATE_CREATED:
+                payment.state = OrderPayment.PAYMENT_STATE_PENDING
+                payment.save()
+            elif data.get('status') in ('expired', 'failed'):
+                payment.state = OrderPayment.PAYMENT_STATE_CANCELED
+                payment.save()
+                payment.order.log_action('pretix_mollie.event.' + data.get('status'))
+            elif data.get('status') == 'paid':
+                payment.order.log_action('pretix_mollie.event.paid')
+                payment.confirm()
+        elif payment.state == OrderPayment.PAYMENT_STATE_CONFIRMED:
+            known_refunds = [r.info_data.get('id') for r in payment.refunds.all()]
+            for r in refunds:
+                if r.get('status') != 'failed' and r.get('id') not in known_refunds:
+                    payment.create_external_refund(
+                        amount=Decimal(r['amount']['value']),
+                        info=json.dumps(r)
+                    )
+            for r in chargebacks:
+                if r.get('id') not in known_refunds:
+                    payment.create_external_refund(
+                        amount=Decimal(r['amount']['value']),
+                        info=json.dumps(r)
+                    )
+    except HTTPError:
+        raise PaymentException(_('We had trouble communicating with Mollie. Please try again and get in touch '
+                                 'with us if this problem persists.'))
+
+
+@event_permission_required('can_change_event_settings')
+@require_POST
+def oauth_disconnect(request, **kwargs):
+    del request.event.settings.payment_mollie_access_token
+    del request.event.settings.payment_mollie_refresh_token
+    del request.event.settings.payment_mollie_expires
+    del request.event.settings.payment_mollie_connect_org_id
+    del request.event.settings.payment_mollie_connect_org_name
+    del request.event.settings.payment_mollie_connect_profiles
+    request.event.settings.payment_mollie__enabled = False
+    messages.success(request, _('Your Mollie account has been disconnected.'))
+
+    return redirect(reverse('control:event.settings.payment.provider', kwargs={
+        'organizer': request.event.organizer.slug,
+        'event': request.event.slug,
+        'provider': 'mollie'
+    }))
+
+
+class MollieOrderView:
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            self.order = request.event.orders.get(code=kwargs['order'])
+            if hashlib.sha1(self.order.secret.lower().encode()).hexdigest() != kwargs['hash'].lower():
+                raise Http404('')
+        except Order.DoesNotExist:
+            # Do a hash comparison as well to harden timing attacks
+            if 'abcdefghijklmnopq'.lower() == hashlib.sha1('abcdefghijklmnopq'.encode()).hexdigest():
+                raise Http404('')
+            else:
+                raise Http404('')
+        return super().dispatch(request, *args, **kwargs)
+
+    @cached_property
+    def payment(self):
+        return get_object_or_404(self.order.payments,
+                                 pk=self.kwargs['payment'],
+                                 provider__startswith='mollie')
+
+    @cached_property
+    def pprov(self):
+        return self.payment.payment_provider
+
+
+@method_decorator(xframe_options_exempt, 'dispatch')
+class ReturnView(MollieOrderView, View):
+    def get(self, request, *args, **kwargs):
+        if self.payment.state not in (OrderPayment.PAYMENT_STATE_CONFIRMED, OrderPayment.PAYMENT_STATE_FAILED,
+                                      OrderPayment.PAYMENT_STATE_CANCELED):
+            try:
+                handle_payment(self.payment, self.payment.info_data.get('id'))
+            except LockTimeoutException:
+                messages.error(self.request, _('We received your payment but were unable to mark your ticket as '
+                                               'the server was too busy. Please check beck in a couple of '
+                                               'minutes.'))
+            except Quota.QuotaExceededException:
+                messages.error(self.request, _('We received your payment but were unable to mark your ticket as '
+                                               'paid as one of your ordered products is sold out. Please contact '
+                                               'the event organizer for further steps.'))
+        return self._redirect_to_order()
+
+    def _redirect_to_order(self):
+        if self.request.session.get('payment_mollie_order_secret') != self.order.secret:
+            messages.error(self.request, _('Sorry, there was an error in the payment process. Please check the link '
+                                           'in your emails to continue.'))
+            return redirect(eventreverse(self.request.event, 'presale:event.index'))
+
+        return redirect(eventreverse(self.request.event, 'presale:event.order', kwargs={
+            'order': self.order.code,
+            'secret': self.order.secret
+        }) + ('?paid=yes' if self.order.status == Order.STATUS_PAID else ''))
+
+
+@method_decorator(csrf_exempt, 'dispatch')
+class WebhookView(View):
+    def post(self, request, *args, **kwargs):
+        try:
+            handle_payment(self.payment, request.POST.get('id'))
+        except LockTimeoutException:
+            return HttpResponse(status=503)
+        except Quota.QuotaExceededException:
+            pass
+        return HttpResponse(status=200)
+
+    @cached_property
+    def payment(self):
+        return get_object_or_404(OrderPayment.objects.filter(order__event=self.request.event),
+                                 pk=self.kwargs['payment'],
+                                 provider__startswith='mollie')
