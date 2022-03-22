8d5a5a11e3a3fd62f4b1541a4545af6535b0a273
Raphael Michel <mail@raphaelmichel.de>
2022-03-22 22:28:26 +0100

    Update plugin metadata

--- pretix_mollie/__init__.py
+++ pretix_mollie/__init__.py
@@ -4,12 +4,15 @@ from django.utils.translation import gettext_lazy
 
 class PluginApp(AppConfig):
     name = 'pretix_mollie'
-    verbose_name = 'Mollie payment integration for pretix'
+    verbose_name = 'Mollie'
 
     class PretixPluginMeta:
         name = gettext_lazy('Mollie')
         author = 'Raphael Michel'
-        description = gettext_lazy('Integration for the Mollie payment provider.')
+        description = gettext_lazy('Accept payments through Mollie, a European payment provider supporting '
+                                   'credit cards as well as many local payment methods such as giropay, '
+                                   'direct debit, iDEAL, wire transfers, and many more.')
+        picture = "pretix_mollie/logo.svg"
         category = 'PAYMENT'
         visible = True
         version = '1.3.2'
