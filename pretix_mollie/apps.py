88628489be895181376b6671d7d800bc78b32110
Raphael Michel <michel@rami.io>
2023-01-30 15:14:21 +0100

    Fix syntax error

--- pretix_mollie/apps.py
+++ pretix_mollie/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy
 from . import __version__
-        compatibility = "pretix>=4.16.0"
 
 
 class PluginApp(AppConfig):
